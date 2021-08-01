1e891d70099e58d6959118949e700a1339495790
Christian Westrom <c.westrom@westrom.xyz>
2021-08-01 01:58:09 -0400

    Add license to source code.

--- test/wildwestrom/lifetime_calendar_test.clj
+++ test/wildwestrom/lifetime_calendar_test.clj
@@ -1,3 +1,19 @@
+;; Lifetime Calendar: See how many days you have left to live at a glance.
+;; Copyright © 2021 Christian Westrom
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 (ns wildwestrom.lifetime-calendar-test
   (:require [clojure.test :refer [deftest testing is]]
             [wildwestrom.lifetime-calendar :as sut]))
