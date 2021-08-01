de15a641d39ff4e4a5a7ca42ebfbeb1c9277d544
Christian Westrom <c.westrom@westrom.xyz>
2021-08-01 01:54:59 -0400

    start project

---
+++ test/wildwestrom/lifetime_calendar_test.clj
@@ -0,0 +1,7 @@
+(ns wildwestrom.lifetime-calendar-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [wildwestrom.lifetime-calendar :as sut]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
