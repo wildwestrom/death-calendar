338bb6734e2db60e3ddcc7e5a30ef910a2d3bc23
Christian Westrom <c.westrom@westrom.xyz>
2021-08-02 20:59:55 -0400

    Added some basic tests for the root

--- src/wildwestrom/death_calendar/backend/core.clj
+++ src/wildwestrom/death_calendar/backend/core.clj
@@ -14,14 +14,13 @@
 ;; You should have received a copy of the GNU Affero General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-(ns wildwestrom.death-calendar.backend.app-test
-  (:require [clojure.test :refer [deftest testing is]]
-            [wildwestrom.death-calendar.backend.app :as sut]
-            [ring.mock.request :as mock]))
+(ns wildwestrom.death-calendar.backend.core
+  (:require [org.httpkit.server :as server]))
 
-(deftest basic-request-response
-  (let [resp (sut/app (mock/request :get "/"))]
-     (testing "Do we get some response?"
-       (is (some? resp)))
-     (testing "Do we get a status 200 back?"
-       (is (= 200 (:status resp))))))
+(def home-body "Hello")
+
+(defn app [request]
+  {:status 200
+   :body home-body
+   :content-type {"html" "text/html"}
+   :charset "UTF8"})
--- test/wildwestrom/death_calendar/backend/core_test.clj
+++ test/wildwestrom/death_calendar/backend/core_test.clj
@@ -13,8 +13,18 @@
 
 ;; You should have received a copy of the GNU Affero General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+(ns wildwestrom.death-calendar.backend.core-test
+  (:require [clojure.test :refer [deftest is testing use-fixtures]]
+            [ring.mock.request :refer [request]]
+            [wildwestrom.death-calendar.backend.core :as sut]))
 
-(ns wildwestrom.death-calendar.backend.app)
-
-(defn app []
-  {:status  200})
+(deftest home-page
+  (let [resp (sut/app (request :get "/"))]
+    (testing "I get a status of 200 when I do a get request on the root."
+      (is (= 200 (:status resp))))
+    (testing "My body has a greeting."
+      (is (= sut/home-body (:body resp))))
+    (testing "The content-type is HTML."
+      (is (= {"html" "text/html"} (:content-type resp))))
+    (testing "It's UTF8."
+      (is (= "UTF8" (:charset resp))))))
