d65944211c00b54fbc55b6f7b293573cf4528c3e
Christian Westrom <c.westrom@westrom.xyz>
2022-07-28 17:19:43 -0400

    refactoring and testing

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,27 @@ struct Args {
     command: Commands,
 }
 
+// This should work for now until https://github.com/clap-rs/clap/issues/1546 is resolved.
+#[derive(Parser, Debug)]
+struct CommonArgs {
+    /// A birthday in `YYYY-MM-DD` format.
+    birthday: Date,
+    /// Expected lifespan in years.
+    #[clap(short, long, default_value_t = 100)]
+    lifespan_years: i16,
+}
+
 #[derive(Parser, Debug)]
 enum Commands {
     /// Print info about your ultimate demise.
     Info {
-        /// A birthday in `YYYY-MM-DD` format.
-        birthday: Date,
-        /// Expected lifespan in years.
-        #[clap(short, long, default_value_t = 100)]
-        lifespan_years: i16,
+        #[clap(flatten)]
+        common_args: CommonArgs,
     },
     /// Generate an SVG Image of the calendar.
     Svg {
-        /// A birthday in `YYYY-MM-DD` format.
-        birthday: Date,
-        /// Expected lifespan in years.
-        #[clap(short, long, default_value_t = 100)]
-        lifespan_years: i16,
+        #[clap(flatten)]
+        common_args: CommonArgs,
         /// Dimensions of your SVG in `WxH` format.
         #[clap(short, long, value_parser, default_value_t = Dimensions {width: 1920, height: 1080})]
         dimensions: Dimensions,
@@ -110,7 +114,12 @@ fn death_info(bday: Date, years: i16) {
 }
 
 fn render_svg(bday: Date, years: i16, d: &Dimensions) -> Document {
-    let mut document = Document::new().set("viewBox", (0, 0, d.width, d.height));
+    let color_primary = "black";
+    let color_secondary = "white";
+
+    let mut document = Document::new()
+        .set("viewBox", (0, 0, d.width, d.height))
+        .set("style", format!("background-color:{}", color_primary));
 
     let today = Date::today_utc();
     let end = death_day(bday, years);
@@ -123,18 +132,23 @@ fn render_svg(bday: Date, years: i16, d: &Dimensions) -> Document {
     let padding = outer_square_size as f64 * 0.2;
 
     while curr_date < end {
-        let fill = if curr_date < today { "black" } else { "white" };
+        let fill = if curr_date < today {
+            color_secondary
+        } else {
+            color_primary
+        };
         let x_offset = (f64::from(d.width) - (outer_square_size * f64::from(years))) / 2.0;
         let x = f64::from(count / weeks_in_year).mul_add(outer_square_size, x_offset);
         let y_offset = (f64::from(d.height) - (outer_square_size * f64::from(weeks_in_year))) / 2.0;
         let y = f64::from(count % weeks_in_year).mul_add(outer_square_size, y_offset);
+        let square_side_size = outer_square_size as f64 - padding;
         let square = Rectangle::new()
             .set("x", x)
             .set("y", y)
-            .set("width", outer_square_size as f64 - padding)
-            .set("height", outer_square_size as f64 - padding)
+            .set("width", square_side_size)
+            .set("height", square_side_size)
             .set("fill", fill)
-            .set("stroke", "black");
+            .set("stroke", color_secondary);
         document.append(square);
         /* All this below is just to make sure there are always 52 weeks. To do this, we change the
         number of days in a week and skip the 29th of February whenever it comes. */
@@ -153,28 +167,42 @@ fn render_svg(bday: Date, years: i16, d: &Dimensions) -> Document {
 
 fn main() {
     // Use these blocks of code for testing purposes.
-    // let args = Args::parse_from(["death-calendar", "info", "1998-07-26", "-l", "100"]);
-    // let args = Args::parse_from(["death-calendar", "svg", "2000-01-01", "-l", "100"]);
     // let args = Args::parse_from(["death-calendar", "--help"]);
+    // let args = Args::parse_from(["death-calendar", "info", "--help"]);
+    // let args = Args::parse_from(["death-calendar", "info", "1998-07-26", "-l", "100"]);
+    // let args = Args::parse_from([
+    //     "death-calendar",
+    //     "svg",
+    //     "2000-01-01",
+    //     "-l",
+    //     "100",
+    //     "-d",
+    //     "2880x1800",
+    //     "-o",
+    //     "test.svg",
+    // ]);
 
     let args = Args::parse();
 
     match args.command {
         Commands::Svg {
-            birthday,
-            lifespan_years,
             dimensions,
             output,
+            // This should work for now until https://github.com/clap-rs/clap/issues/1546 is resolved.
+            common_args,
         } => {
-            let document = render_svg(birthday, lifespan_years, &dimensions);
+            let document = render_svg(
+                common_args.birthday,
+                common_args.lifespan_years,
+                &dimensions,
+            );
             output.map_or_else(
                 || println!("{}", document.to_string()),
                 |file| svg::save(file, &document).expect("Couldn't save SVG to file."),
             );
         }
-        Commands::Info {
-            birthday,
-            lifespan_years,
-        } => death_info(birthday, lifespan_years),
+        Commands::Info { common_args } => {
+            death_info(common_args.birthday, common_args.lifespan_years)
+        }
     }
 }
