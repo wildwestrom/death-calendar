9229f2e7d58633f60d4a349c06c2eea75b0d8fa4
Christian Westrom <c.westrom@westrom.xyz>
2022-08-13 20:26:42 -0400

    get rid of dimensions parsing and calculate svg with integers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::{error::Error, fmt, num::ParseIntError, path::PathBuf};
+use std::path::PathBuf;
 
-use clap::Parser;
 use death_calendar::{
     days_left, days_lived, death_day, lifespan_days, lifespan_weeks, weeks_left, weeks_lived,
     years_left, years_lived,
 };
+
+use clap::Parser;
 use gregorian::Date;
 use svg::{node::element::Rectangle, Document, Node};
 
@@ -25,6 +26,7 @@ struct CommonArgs {
     lifespan_years: i16,
 }
 
+// This won't work until https://github.com/clap-rs/clap/issues/1546 is fixed.
 #[derive(Parser, Debug)]
 enum Commands {
     /// Print info about your ultimate demise.
@@ -37,62 +39,9 @@ enum Commands {
         #[clap(flatten)]
         common_args: CommonArgs,
         /// Dimensions of your SVG in `WxH` format.
-        #[clap(short, long, value_parser, default_value_t = Dimensions {width: 1920, height: 1080})]
-        dimensions: Dimensions,
-        /// Save SVG to a file instead of printing to stdout.
         #[clap(short, long)]
         output: Option<PathBuf>,
     },
-}
-
-#[derive(Debug, Clone)]
-struct Dimensions {
-    width: i16,
-    height: i16,
-}
-
-impl fmt::Display for Dimensions {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!("{}x{}", self.width, self.height))
-    }
-}
-
-impl Default for Dimensions {
-    fn default() -> Self {
-        Self {
-            width: Default::default(),
-            height: Default::default(),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct ParseDimensionsError;
-
-impl Error for ParseDimensionsError {}
-
-impl fmt::Display for ParseDimensionsError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("Could not parse input dimensions. Expected format `WxH`.")
-    }
-}
-
-impl From<ParseIntError> for ParseDimensionsError {
-    fn from(_: ParseIntError) -> Self {
-        ParseDimensionsError
-    }
-}
-
-impl std::str::FromStr for Dimensions {
-    type Err = ParseDimensionsError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let tokens: Vec<&str> = s.split('x').collect();
-        let width = i16::from_str(tokens.first().ok_or(ParseDimensionsError)?)?;
-        let height = i16::from_str(tokens.last().ok_or(ParseDimensionsError)?)?;
-
-        Ok(Self { width, height })
-    }
 }
 
 fn death_info(bday: Date, years: i16) {
@@ -113,13 +62,9 @@ fn death_info(bday: Date, years: i16) {
     println!("- {} years", years_left(today, bday, years).abs());
 }
 
-fn render_svg(bday: Date, years: i16, d: &Dimensions) -> Document {
+fn render_svg(bday: Date, years: i16) -> Document {
     let color_primary = "black";
     let color_secondary = "white";
-
-    let mut document = Document::new()
-        .set("viewBox", (0, 0, d.width, d.height))
-        .set("style", format!("background-color:{}", color_primary));
 
     let today = Date::today_utc();
     let end = death_day(bday, years);
@@ -128,27 +73,48 @@ fn render_svg(bday: Date, years: i16, d: &Dimensions) -> Document {
 
     let mut count = 0;
     let mut curr_date = bday;
-    let outer_square_size: f64 = (d.height / weeks_in_year).min(d.width / years).into();
-    let padding = outer_square_size as f64 * 0.2;
+    let inner_square_size = 100;
+    let padding = 20;
+    let outer_square_size = inner_square_size + padding;
+
+    let border_size = 2;
+    let grid_width = outer_square_size * years;
+    let grid_height = outer_square_size * weeks_in_year;
+    let viewbox_width = years * outer_square_size + outer_square_size * border_size;
+    let viewbox_height = weeks_in_year * outer_square_size + outer_square_size * border_size;
+
+    let mut document = Document::new()
+        .set("viewBox", (0, 0, viewbox_width, viewbox_height))
+        .set("style", format!("background-color:{}", color_primary));
+
+    let background = Rectangle::new()
+        .set("x", 0)
+        .set("y", 0)
+        .set("width", viewbox_width)
+        .set("height", viewbox_height)
+        .set("fill", color_secondary);
+
+    document.append(background);
 
     while curr_date < end {
         let fill = if curr_date < today {
-            color_secondary
-        } else {
             color_primary
+        } else {
+            color_secondary
         };
-        let x_offset = (f64::from(d.width) - (outer_square_size * f64::from(years))) / 2.0;
-        let x = f64::from(count / weeks_in_year).mul_add(outer_square_size, x_offset);
-        let y_offset = (f64::from(d.height) - (outer_square_size * f64::from(weeks_in_year))) / 2.0;
-        let y = f64::from(count % weeks_in_year).mul_add(outer_square_size, y_offset);
-        let square_side_size = outer_square_size as f64 - padding;
+        let x_offset = (viewbox_width - grid_width) / 2;
+        let x = ((count / weeks_in_year) * outer_square_size) + x_offset;
+        let y_offset = (viewbox_height - grid_height) / 2;
+        let y = ((count % weeks_in_year) * outer_square_size) + y_offset;
         let square = Rectangle::new()
             .set("x", x)
             .set("y", y)
-            .set("width", square_side_size)
-            .set("height", square_side_size)
+            .set("width", inner_square_size)
+            .set("height", inner_square_size)
             .set("fill", fill)
-            .set("stroke", color_secondary);
+            .set("stroke", color_primary)
+            .set("stroke-width", padding / 4);
+
         document.append(square);
         /* All this below is just to make sure there are always 52 weeks. To do this, we change the
         number of days in a week and skip the 29th of February whenever it comes. */
@@ -176,8 +142,6 @@ fn main() {
     //     "2000-01-01",
     //     "-l",
     //     "100",
-    //     "-d",
-    //     "2880x1800",
     //     "-o",
     //     "test.svg",
     // ]);
@@ -186,16 +150,11 @@ fn main() {
 
     match args.command {
         Commands::Svg {
-            dimensions,
             output,
             // This should work for now until https://github.com/clap-rs/clap/issues/1546 is resolved.
             common_args,
         } => {
-            let document = render_svg(
-                common_args.birthday,
-                common_args.lifespan_years,
-                &dimensions,
-            );
+            let document = render_svg(common_args.birthday, common_args.lifespan_years);
             output.map_or_else(
                 || println!("{}", document.to_string()),
                 |file| svg::save(file, &document).expect("Couldn't save SVG to file."),
