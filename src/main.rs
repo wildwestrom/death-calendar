411fcadf11a3cb320d601b24b2587464ed6264b0
Christian Westrom <c.westrom@westrom.xyz>
2022-08-13 21:19:38 -0400

    fix clippy issues

--- src/main.rs
+++ src/main.rs
@@ -62,6 +62,7 @@ fn death_info(bday: Date, years: i16) {
     println!("- {} years", years_left(today, bday, years).abs());
 }
 
+const WEEKS_IN_A_YEAR: i16 = 52;
 fn render_svg(bday: Date, years: i16) -> Document {
     let color_primary = "black";
     let color_secondary = "white";
@@ -69,19 +70,20 @@ fn render_svg(bday: Date, years: i16) -> Document {
     let today = Date::today_utc();
     let end = death_day(bday, years);
 
-    let weeks_in_year = 52;
-
     let mut count = 0;
     let mut curr_date = bday;
-    let inner_square_size = 100;
-    let padding = 20;
-    let outer_square_size = inner_square_size + padding;
+
+    // Adding a scale factor seems to make the image render more crisply.
+    let scale_factor = 4;
+    let inner_square_size = 12 * scale_factor;
+    let padding = 1 * scale_factor;
+    let outer_square_size = inner_square_size + (padding * 3);
 
     let border_size = 2;
     let grid_width = outer_square_size * years;
-    let grid_height = outer_square_size * weeks_in_year;
+    let grid_height = outer_square_size * WEEKS_IN_A_YEAR;
     let viewbox_width = years * outer_square_size + outer_square_size * border_size;
-    let viewbox_height = weeks_in_year * outer_square_size + outer_square_size * border_size;
+    let viewbox_height = WEEKS_IN_A_YEAR * outer_square_size + outer_square_size * border_size;
 
     let mut document = Document::new()
         .set("viewBox", (0, 0, viewbox_width, viewbox_height))
@@ -103,9 +105,9 @@ fn render_svg(bday: Date, years: i16) -> Document {
             color_secondary
         };
         let x_offset = (viewbox_width - grid_width) / 2;
-        let x = ((count / weeks_in_year) * outer_square_size) + x_offset;
+        let x = ((count / WEEKS_IN_A_YEAR) * outer_square_size) + x_offset;
         let y_offset = (viewbox_height - grid_height) / 2;
-        let y = ((count % weeks_in_year) * outer_square_size) + y_offset;
+        let y = ((count % WEEKS_IN_A_YEAR) * outer_square_size) + y_offset;
         let square = Rectangle::new()
             .set("x", x)
             .set("y", y)
@@ -113,7 +115,7 @@ fn render_svg(bday: Date, years: i16) -> Document {
             .set("height", inner_square_size)
             .set("fill", fill)
             .set("stroke", color_primary)
-            .set("stroke-width", padding / 4);
+            .set("stroke-width", padding);
 
         document.append(square);
         /* All this below is just to make sure there are always 52 weeks. To do this, we change the
@@ -156,12 +158,12 @@ fn main() {
         } => {
             let document = render_svg(common_args.birthday, common_args.lifespan_years);
             output.map_or_else(
-                || println!("{}", document.to_string()),
+                || println!("{}", document),
                 |file| svg::save(file, &document).expect("Couldn't save SVG to file."),
             );
         }
         Commands::Info { common_args } => {
-            death_info(common_args.birthday, common_args.lifespan_years)
+            death_info(common_args.birthday, common_args.lifespan_years);
         }
     }
 }
